ac4d37149bdd2a5e88f0a39f76881b9fe75ad133
luisthieme <luis.thieme@5minds.de>
2024-06-14 19:47:30 +0200

    fix build error

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "@/utils/AuthOptions";
 import User from "@/components/User";
 
 export default async function Page() {
--- src/app/serversideprotection/page.tsx
+++ src/app/serversideprotection/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "@/utils/AuthOptions";
 import NotAuthorized from "@/components/NotAuthorized";
 import ProtectedContent from "@/components/ProtectedContent";
 
